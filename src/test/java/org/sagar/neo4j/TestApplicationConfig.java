5e227dd8f00738e24d3f7270bbef7c8d48952f23
Sagar Prasad <sagar.prasad@gmail.com>
2014-03-14 12:45:41 +0530

    Changing scope to singleton for graphDB service
    Adding shutdown hook

--- src/test/java/org/sagar/neo4j/TestApplicationConfig.java
+++ src/test/java/org/sagar/neo4j/TestApplicationConfig.java
@@ -1,35 +1,44 @@
 package org.sagar.neo4j;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
+
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.kernel.impl.util.FileUtils;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Scope;
-import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.io.File;
-import java.io.IOException;
-
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
-
 /**
- * @author mh
- * @since 01.06.12
+ * @author Sagar
  */
 @Configuration
 @EnableTransactionManagement
-@EnableNeo4jRepositories(basePackages = "org.sagar.neo4j")
-public class TestApplicationConfig extends ApplicationConfig {
-    @Bean(destroyMethod = "shutdown")
-    @Scope(SCOPE_PROTOTYPE)
-    public GraphDatabaseService graphDatabaseService() {
-        try {
-            FileUtils.deleteRecursively(new File("target/test-db"));
-            return new EmbeddedGraphDatabase("target/test-db");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//@EnableNeo4jRepositories(basePackages = "org.sagar.neo4j")
+@ImportResource("classpath:META-INF/spring/test-data-context.xml")
+public class TestApplicationConfig {
+  
+   @Bean(destroyMethod = "shutdown")
+   @Scope(SCOPE_SINGLETON)
+   public GraphDatabaseService graphDatabaseService() {
+	    System.out.println("**************************CREATING DB**************************"); 
+    	GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( "target/test-db" );
+    	registerShutdownHook( graphDb );
+    	System.out.println("**************************REGISTED SHUT HOOK**************************");
+    	return graphDb;
     }
+   
+   private static void registerShutdownHook( final GraphDatabaseService graphDb )
+   {
+       Runtime.getRuntime().addShutdownHook( new Thread()
+       {
+           @Override
+           public void run()
+           {
+        	   System.out.println("**************************SHUTTING DOWN DB**************************");
+               graphDb.shutdown();
+           }
+       } );
+   }
 }
